1dea0932b283825d90247cab905a687fefecdb7e
SrivatsanV <srivi567@gmail.com>
2020-07-10 14:35:33 +0530

    Resolved issues wiht mutlti page access

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 const port = process.env.PORT || 8000;
